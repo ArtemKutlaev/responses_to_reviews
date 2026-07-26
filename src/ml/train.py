ced4aa2089d50193267ec4d7f9b1666ded8a04fa
Artem_Kutlaev <kutlaevartem257@gmail.com>
2026-07-26 21:20:40 +0300

    feat:Добавление метрик f-1, classification_report и матрицы ошибок для осознания слабостей модели

--- src/ml/train.py
+++ src/ml/train.py
@@ -1,18 +1,48 @@
 from sklearn.model_selection import train_test_split
 from src.ml.prepare_data import get_prepared_data
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix,ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 from src.config_path import MODEL_PATH,VECTORIZER_PATH
+import matplotlib.pyplot as plt
+
 
 X,y,vectorizer,comments = get_prepared_data()
-
-X_train,X_test,y_train,y_test,comments_train,comments_test = train_test_split(X,y,comments,test_size=0.3,random_state=1)
+print(y.value_counts().sort_index())
+X_train,X_test,y_train,y_test,comments_train,comments_test = train_test_split(X,y,comments,test_size=0.3,random_state=1,stratify=y)
 model = RandomForestClassifier(n_estimators=100,max_depth=100,random_state=1,n_jobs=-1)
 model.fit(X_train,y_train)
 y_pred = model.predict(X_test)
+
 accuracy = accuracy_score(y_test,y_pred)
+report = classification_report(
+    y_true=y_test,
+    y_pred=y_pred,
+    labels =[0,1,2],
+    target_names=[
+        "negative",
+        "neutral",
+        "positive"
+    ],
+    digits=4,
+    zero_division=0
+)
+
+c_matrix = confusion_matrix(
+    y_true = y_test,
+    y_pred=y_pred,
+    labels=[0,1,2]
+)
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=c_matrix,display_labels=["negative","neutral","positive"]
+)
+
+
 print(accuracy)
+print(report)
+disp.plot(cmap='Blues')
+plt.title('Матрица ошибок по отзывам WB')
+plt.show()
 
 joblib.dump(model,MODEL_PATH)
 joblib.dump(vectorizer,VECTORIZER_PATH)
